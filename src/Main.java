71524b58bfbe85e2d29c3d0fbe98ba0682c808a3
Maxim Ponomarev <ponomarevmv8@yandex.ru>
2023-02-11 23:15:29 +0300

    feat: add Start class
    Добавление класса для старта программы и выбора режима работы.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-
+import java.io.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
+    private static final String beginning= "######----Encrypt/Decrip Cheaser Chiper's----######\n" +
+                                    "               Welcome to uor programm                       \n";
 
+
+    public static void main(String[] args) {
+
+        System.out.println(beginning);
+
+        Start start = new Start();
+        int selectMode = start.selectMode();
+        if(selectMode == 1) {
+
+        } else {
+
+        }
+
+
+    }
 }
---
+++ src/Start.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Start {
+
+    private final String alphabet ="абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,\":-!? ";
+
+    private final Map<Integer, List<Character>> shifr = new HashMap<>();
+
+    private List<Character> alphabetShifr = new ArrayList<>(alphabet.length());
+
+    private Scanner scanner;
+
+    public Start(){
+        for(int i = 0; i < alphabet.length(); i++) {
+            for(int j = 0; j < alphabet.length(); j++) {
+                alphabetShifr.add(alphabet.charAt((j+i) % alphabet.length()));
+            }
+            shifr.put(i, alphabetShifr);
+            alphabetShifr.clear();
+        }
+
+    }
+
+
+    public int selectMode() {
+        System.out.println("          Выберите режим работы программы\n               1. Расшифровка\n               2. Зашифровка\n");
+        while (true){
+            System.out.print("Выбранный режим: ");
+            scanner = new Scanner(System.in);
+            while (scanner.hasNextInt()){
+                int i = scanner.nextInt();
+                if(i == 1) {
+                    return 1;
+                } else if (i == 2) {
+                    return 2;
+                } else {
+                    System.out.println("Вы выбрали не существующий режим");
+                    System.out.print("Выбранный режим: ");
+                }
+            }
+        }
+    }
+
+    public Map<Integer, List<Character>> getShifr() {
+        return shifr;
+    }
+}
